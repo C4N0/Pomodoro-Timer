8817c3bac564b3743b6271d7aa599551f3871eca
C4N0 <76574048+C4N0@users.noreply.github.com>
2021-11-09 23:29:03 +0100

    Update pomodorotimer.py

--- pomodorotimer.py
+++ pomodorotimer.py
@@ -10,26 +10,26 @@ import time
 from datetime import datetime
 
 now = datetime.now()
-dt_string = now.strftime("%H:%M:%S")
-print(f"your challenge started at {dt_string}")
+dt_string = now.strftime("%H:%M:%S") # gives you the exact time
+print(f"your challenge started at {dt_string}") # tels you when the challenge starts
 
-seconds = 1500
+seconds = 1501 # pomodoro time in seconds (25*60 = 1500)
 
 for sec in range(seconds):
     
     break_time = str(seconds - sec) 
     
-    time.sleep(1)
+    time.sleep(1) # counts the seconds down in 1 second intervals
     
     if break_time == "1": 
-        chess = webbrowser.open("Link to your website (i.e. lichess.org")
+        webpage = webbrowser.open("Link to your website (i.e. lichess.org") # opens website to announce break time
 
 break_sec = 401
 
 for sec in range(break_sec): 
     break_end = str(break_sec - sec)
     
-    time.sleep(1)
+    time.sleep(1) # same as above only with your break time
     
     if break_end == "1":
         
@@ -40,6 +40,6 @@ for sec in range(break_sec):
         eren = "path to your soundbite"
         pain = "path to your soundbite"
         ougon = "path to your soundbite"
-        list_of_sounds = [luffy, allmight, onepiece_overtake, grandline, eren, pain, ougon]
+        list_of_sounds = [luffy, allmight, onepiece_overtake, grandline, eren, pain, ougon] # saves the soundbites in a list to randomly choose from later
         
-        playsound.playsound(random.choice(list_of_sounds))
+        playsound.playsound(random.choice(list_of_sounds)) # plays random soundbite
